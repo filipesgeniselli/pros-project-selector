5484cc466b48905a2bc14b3536809e97e48d643c
Filipe Geniselli <filipe.sgeniselli@gmail.com>
2021-04-15 20:39:32 -0300

    feat: project setup

---
+++ src/main/java/br/com/filisg/prosprojectselector/ProsProjectSelectorApplication.java
@@ -0,0 +1,13 @@
+package br.com.filisg.prosprojectselector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProsProjectSelectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProsProjectSelectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/filisg/prosprojectselector/ProsProjectSelectorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.filisg.prosprojectselector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProsProjectSelectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
